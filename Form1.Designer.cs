01099ac7256fda51a6b5ada88f1774cbfbec9ba4
WALEED BIN NAEEM <134872754+waleedbinnaeem7@users.noreply.github.com>
2024-01-08 21:49:04 +0500

    Tic-tac-toe
    Tic-tac-toe in C# (Windows Form)

---
+++ Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace Tic_tac_toe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnx = new System.Windows.Forms.Button();
+            this.btno = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.A1 = new System.Windows.Forms.Button();
+            this.B1 = new System.Windows.Forms.Button();
+            this.C1 = new System.Windows.Forms.Button();
+            this.A2 = new System.Windows.Forms.Button();
+            this.B2 = new System.Windows.Forms.Button();
+            this.C2 = new System.Windows.Forms.Button();
+            this.A3 = new System.Windows.Forms.Button();
+            this.B3 = new System.Windows.Forms.Button();
+            this.C3 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnx
+            // 
+            this.btnx.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnx.ForeColor = System.Drawing.Color.Red;
+            this.btnx.Location = new System.Drawing.Point(105, 43);
+            this.btnx.Name = "btnx";
+            this.btnx.Size = new System.Drawing.Size(40, 34);
+            this.btnx.TabIndex = 0;
+            this.btnx.Text = "x";
+            this.btnx.UseVisualStyleBackColor = true;
+            this.btnx.Click += new System.EventHandler(this.btnx_Click);
+            // 
+            // btno
+            // 
+            this.btno.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btno.Location = new System.Drawing.Point(166, 43);
+            this.btno.Name = "btno";
+            this.btno.Size = new System.Drawing.Size(39, 34);
+            this.btno.TabIndex = 1;
+            this.btno.Text = "o";
+            this.btno.UseVisualStyleBackColor = true;
+            this.btno.Click += new System.EventHandler(this.btno_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "players";
+            // 
+            // A1
+            // 
+            this.A1.Enabled = false;
+            this.A1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.A1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.A1.Location = new System.Drawing.Point(22, 111);
+            this.A1.Name = "A1";
+            this.A1.Size = new System.Drawing.Size(63, 61);
+            this.A1.TabIndex = 3;
+            this.A1.UseVisualStyleBackColor = true;
+            this.A1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // B1
+            // 
+            this.B1.Enabled = false;
+            this.B1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.B1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.B1.Location = new System.Drawing.Point(91, 111);
+            this.B1.Name = "B1";
+            this.B1.Size = new System.Drawing.Size(63, 61);
+            this.B1.TabIndex = 4;
+            this.B1.UseVisualStyleBackColor = true;
+            this.B1.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // C1
+            // 
+            this.C1.Enabled = false;
+            this.C1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.C1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.C1.Location = new System.Drawing.Point(160, 111);
+            this.C1.Name = "C1";
+            this.C1.Size = new System.Drawing.Size(63, 61);
+            this.C1.TabIndex = 5;
+            this.C1.UseVisualStyleBackColor = true;
+            this.C1.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // A2
+            // 
+            this.A2.Enabled = false;
+            this.A2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.A2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.A2.Location = new System.Drawing.Point(22, 178);
+            this.A2.Name = "A2";
+            this.A2.Size = new System.Drawing.Size(63, 61);
+            this.A2.TabIndex = 6;
+            this.A2.UseVisualStyleBackColor = true;
+            this.A2.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // B2
+            // 
+            this.B2.Enabled = false;
+            this.B2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.B2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.B2.Location = new System.Drawing.Point(91, 178);
+            this.B2.Name = "B2";
+            this.B2.Size = new System.Drawing.Size(63, 61);
+            this.B2.TabIndex = 7;
+            this.B2.UseVisualStyleBackColor = true;
+            this.B2.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // C2
+            // 
+            this.C2.Enabled = false;
+            this.C2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.C2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.C2.Location = new System.Drawing.Point(160, 178);
+            this.C2.Name = "C2";
+            this.C2.Size = new System.Drawing.Size(63, 61);
+            this.C2.TabIndex = 8;
+            this.C2.UseVisualStyleBackColor = true;
+            this.C2.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // A3
+            // 
+            this.A3.Enabled = false;
+            this.A3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.A3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.A3.Location = new System.Drawing.Point(22, 247);
+            this.A3.Name = "A3";
+            this.A3.Size = new System.Drawing.Size(63, 61);
+            this.A3.TabIndex = 9;
+            this.A3.UseVisualStyleBackColor = true;
+            this.A3.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // B3
+            // 
+            this.B3.Enabled = false;
+            this.B3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.B3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.B3.Location = new System.Drawing.Point(91, 247);
+            this.B3.Name = "B3";
+            this.B3.Size = new System.Drawing.Size(63, 61);
+            this.B3.TabIndex = 10;
+            this.B3.UseVisualStyleBackColor = true;
+            this.B3.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // C3
+            // 
+            this.C3.Enabled = false;
+            this.C3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.C3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.C3.Location = new System.Drawing.Point(160, 247);
+            this.C3.Name = "C3";
+            this.C3.Size = new System.Drawing.Size(63, 61);
+            this.C3.TabIndex = 11;
+            this.C3.UseVisualStyleBackColor = true;
+            this.C3.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(32, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(71, 23);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "start/restart\r\n\r\n\r\n";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(244, 320);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.C3);
+            this.Controls.Add(this.B3);
+            this.Controls.Add(this.A3);
+            this.Controls.Add(this.C2);
+            this.Controls.Add(this.B2);
+            this.Controls.Add(this.A2);
+            this.Controls.Add(this.C1);
+            this.Controls.Add(this.B1);
+            this.Controls.Add(this.A1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btno);
+            this.Controls.Add(this.btnx);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnx;
+        private System.Windows.Forms.Button btno;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button A1;
+        private System.Windows.Forms.Button B1;
+        private System.Windows.Forms.Button C1;
+        private System.Windows.Forms.Button A2;
+        private System.Windows.Forms.Button B2;
+        private System.Windows.Forms.Button C2;
+        private System.Windows.Forms.Button A3;
+        private System.Windows.Forms.Button B3;
+        private System.Windows.Forms.Button C3;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,380 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tic_tac_toe
+{
+    public partial class Form1 : Form
+    {
+
+        int _isgameover = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void refresh()
+        {
+
+            A1.Enabled = false;
+            A2.Enabled = false;
+            A3.Enabled = false;
+            B1.Enabled = false;
+            B2.Enabled = false;
+            B3.Enabled = false;
+            C1.Enabled = false;
+            C2.Enabled = false;
+            C3.Enabled = false;
+      
+          
+           
+
+        }
+        
+
+        private void showmsg()
+        {
+            if (A1.Text == "" && A2.Text == "" && A3.Text == "")
+            {
+
+            }
+            else
+            {
+                if (A1.Text == A2.Text && A2.Text == A3.Text)
+                {
+                    MessageBox.Show("Winner is " + A1.Text + "");
+                    _isgameover = 1;
+                    refresh();
+                }
+
+            }
+            if (B1.Text == "" && B2.Text == "" && B3.Text == "")
+            {
+
+            }
+            else
+            {
+
+                if (B1.Text == B2.Text && B2.Text == B3.Text)
+                {
+                    MessageBox.Show("Winner is" + B2.Text + " ");
+                    _isgameover = 1;
+                    refresh();
+                }
+            }
+            if (C1.Text == "" && C2.Text == "" && C3.Text == "")
+            {
+
+            }
+            else
+            {
+                if (C1.Text == C2.Text && C2.Text == C3.Text)
+                {
+                    MessageBox.Show("Winner is" + C1.Text + "");
+                    _isgameover = 1;
+                    refresh();
+                }
+            }
+            if (A1.Text == "" && B2.Text == "" && C3.Text == "")
+            {
+
+            }
+            else
+            {
+                if (A1.Text == B2.Text && B2.Text == C3.Text)
+                {
+                    MessageBox.Show("Winner is" + A1.Text + "");
+                    _isgameover = 1;
+                    refresh();
+                }
+            }
+            if (C1.Text == "" && B2.Text == "" && A3.Text == "")
+            {
+
+            }
+            else
+            {
+                if (C1.Text == B2.Text && B2.Text == A3.Text)
+                {
+                    MessageBox.Show("Winner is" + A3.Text + "");
+                    _isgameover = 1;
+                    refresh();
+                }
+            }
+            if (A1.Text == "" && B1.Text == "" && C1.Text == "")
+            {
+
+            }
+            else
+            {
+                if (A1.Text == B1.Text && B1.Text == C1.Text)
+                {
+                    MessageBox.Show("Winner is" + A1.Text + "");
+                    _isgameover = 1;
+                    refresh();
+                }
+            }
+            if (A2.Text == "" && B2.Text == "" && C2.Text == "")
+            {
+
+            }
+            else
+            {
+                if (A2.Text == B2.Text && B2.Text == C2.Text)
+                {
+                    MessageBox.Show("Winner is" + A2.Text + "");
+                    _isgameover = 1;
+                    refresh();
+                }
+            } if (A3.Text == "" && B3.Text == "" && C3.Text == "")
+            {
+
+            }
+            else
+            {
+                if (A3.Text == B3.Text && B3.Text == C3.Text)
+                {
+                    MessageBox.Show("Winner is" + A3.Text + "");
+                    _isgameover = 1;
+                    refresh();
+                }
+            }
+
+            if (A1.Text != "" && A2.Text != "" && A3.Text != "" && B1.Text != "" && B2.Text != "" && B3.Text != "" && C1.Text != "" && C2.Text != "" && C3.Text != "")
+            if(_isgameover==0)
+            {
+                MessageBox.Show("Game Over!");
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnx_Click(object sender, EventArgs e)
+        {
+            if (btno.ForeColor == Color.Red)
+            {
+                btnx.ForeColor = Color.Red;
+                btno.ForeColor = Color.Black;
+            }
+        }
+
+        private void btno_Click(object sender, EventArgs e)
+        {
+            if (btnx.ForeColor == Color.Red)
+            {
+                btnx.ForeColor = Color.Black;
+                btno.ForeColor = Color.Red;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (btnx.ForeColor == Color.Red)
+            {
+                A1.Text = "X";
+                A1.Enabled = false;
+                btno.ForeColor = Color.Red;
+                btnx.ForeColor = Color.Black;
+            }
+            else
+            {
+                A1.Text = "O";
+                A1.Enabled = false;
+                btnx.ForeColor = Color.Red;
+                btno.ForeColor = Color.Black;
+            }
+            showmsg();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (btnx.ForeColor == Color.Red)
+            {
+                B1.Text = "X";
+                B1.Enabled = false;
+                btno.ForeColor = Color.Red;
+                btnx.ForeColor = Color.Black;
+            }
+            else
+            {
+                B1.Text = "O";
+                B1.Enabled = false;
+                btnx.ForeColor = Color.Red;
+                btno.ForeColor = Color.Black;
+            }
+            showmsg();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (btnx.ForeColor == Color.Red)
+            {
+                C1.Text = "X";
+                C1.Enabled = false;
+                btno.ForeColor = Color.Red;
+                btnx.ForeColor = Color.Black;
+            }
+            else
+            {
+                C1.Text = "O";
+                C1.Enabled = false;
+                btnx.ForeColor = Color.Red;
+                btno.ForeColor = Color.Black;
+            }
+            showmsg();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (btnx.ForeColor == Color.Red)
+            {
+                A2.Text = "X";
+                A2.Enabled = false;
+                btno.ForeColor = Color.Red;
+                btnx.ForeColor = Color.Black;
+            }
+            else
+            {
+                A2.Text = "O";
+                A2.Enabled = false;
+                btnx.ForeColor = Color.Red;
+                btno.ForeColor = Color.Black;
+            }
+            showmsg();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (btnx.ForeColor == Color.Red)
+            {
+                B2.Text = "X";
+                B2.Enabled = false;
+                btno.ForeColor = Color.Red;
+                btnx.ForeColor = Color.Black;
+            }
+            else
+            {
+                B2.Text = "O";
+                B2.Enabled = false;
+                btnx.ForeColor = Color.Red;
+                btno.ForeColor = Color.Black;
+            }
+            showmsg();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (btnx.ForeColor == Color.Red)
+            {
+                C2.Text = "X";
+                C2.Enabled = false;
+                btno.ForeColor = Color.Red;
+                btnx.ForeColor = Color.Black;
+            }
+            else
+            {
+                C2.Text = "O";
+                C2.Enabled = false;
+                btnx.ForeColor = Color.Red;
+                btno.ForeColor = Color.Black;
+            }
+            showmsg();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (btnx.ForeColor == Color.Red)
+            {
+                A3.Text = "X";
+                A3.Enabled = false;
+                btno.ForeColor = Color.Red;
+                btnx.ForeColor = Color.Black;
+            }
+            else
+            {
+                A3.Text = "O";
+                A3.Enabled = false;
+                btnx.ForeColor = Color.Red;
+                btno.ForeColor = Color.Black;
+            }
+            showmsg();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (btnx.ForeColor == Color.Red)
+            {
+                B3.Text = "X";
+                B3.Enabled = false;
+                btno.ForeColor = Color.Red;
+                btnx.ForeColor = Color.Black;
+            }
+            else
+            {
+                B3.Text = "O";
+                B3.Enabled = false;
+                btnx.ForeColor = Color.Red;
+                btno.ForeColor = Color.Black;
+            }
+            showmsg();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (btnx.ForeColor == Color.Red)
+            {
+                C3.Text = "X";
+                C3.Enabled = false;
+                btno.ForeColor = Color.Red;
+                btnx.ForeColor = Color.Black;
+            }
+            else
+            {
+                C3.Text = "O";
+                C3.Enabled = false;
+                btnx.ForeColor = Color.Red;
+                btno.ForeColor = Color.Black;
+            }
+            showmsg();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            A1.Enabled = true;
+            A2.Enabled = true;
+            A3.Enabled = true;
+            B1.Enabled = true;
+            B2.Enabled = true;
+            B3.Enabled = true;
+            C1.Enabled = true;
+            C2.Enabled = true;
+            C3.Enabled = true;
+            A1.Text = "";
+            A2.Text = "";
+            A3.Text = "";
+            B1.Text = "";
+            B2.Text = "";
+            B3.Text = "";
+            C1.Text = "";
+            C2.Text = "";
+            C3.Text = "";
+        }
+    }
+}
